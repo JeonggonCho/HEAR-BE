fa3a2c5c7bb0d4f89b3f444a4daf6b2fc6a50934
조정곤 <wjdrhs62@gmail.com>
2024-09-05 02:26:08 +0900

    fix: JWT 유틸함수 수정

--- src/utils/jwtUtil.ts
+++ src/utils/jwtUtil.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 import RefreshTokenModel from "../models/refreshTokenModel";
+import UserModel from "../models/userModel";
 import HttpError from "../models/errorModel";
 
 dotenv.config();
@@ -61,38 +62,37 @@ const jwtUtil = {
             const token = jwt.sign({}, JWT_PRIVATE_KEY, {expiresIn: "14d"});
 
             // 리프레쉬 토큰 저장
-            const createdRefreshToken = await RefreshTokenModel.create([{userId, token}], {sess});
+            const createdRefreshToken = await RefreshTokenModel.create([{userId, token}], {session: sess});
+
+            // 유저의 refreshTokenId를 업데이트
+            await UserModel.updateOne(
+                {_id: userId},
+                {$set: {refreshTokenId: createdRefreshToken[0]._id}}
+            ).session(sess);
 
             // 트랜잭션 커밋
             await sess.commitTransaction();
-            await sess.endSession();
 
             // 토큰과 저장된 리프레쉬 토큰의 _id 반환
             return [token, createdRefreshToken[0]._id as mongoose.Types.ObjectId];
         } catch (err) {
             await sess.abortTransaction();
-            await sess.endSession();
-
             throw new HttpError("토큰 갱신 중 오류가 발생했습니다.", 500);
+        } finally {
+            await sess.endSession();
         }
     },
 
     // 리프레쉬 토큰 검증
     refreshVerify: async (refreshToken: string, userId: string) => {
-        let existingRefreshToken;
         try {
-            existingRefreshToken = await RefreshTokenModel.findOne({userId, token: refreshToken});
-
+            const existingRefreshToken = await RefreshTokenModel.findOne({userId, token: refreshToken});
             if (!existingRefreshToken) {
                 return false;
             }
 
-            try {
-                jwt.verify(refreshToken, JWT_PRIVATE_KEY);
-                return true;
-            } catch (err) {
-                return false;
-            }
+            jwt.verify(refreshToken, JWT_PRIVATE_KEY);
+            return true;
         } catch (err) {
             return false;
         }
